446b22382508805f89847286b87e15c6946f6be1
Premchand Itham <161148138+prem096@users.noreply.github.com>
2024-10-03 22:20:30 +0530

    Update 10-rockpaperscisssor.js

--- 10-rockpaperscisssor.js
+++ 10-rockpaperscisssor.js
@@ -1,108 +1,73 @@
 let score = JSON.parse(localStorage.getItem('score')) || {
-  wins : 0,
-  losses : 0,
-  ties : 0
+  wins: 0,
+  losses: 0,
+  ties: 0
 };
 
 updateScore();
-/*
 
-if (!score){
-score={
-  wins : 0,
-  losses : 0,
-  ties : 0
-};
-}
-*/
+function playerMove(Move) {
+  const computerMove = computerMoves();
+  let result = '';
 
+  if (Move === 'scissor') {
+    if (computerMove === 'rock') {
+      result = 'You Lose.';
+    } else if (computerMove === 'paper') {
+      result = 'You Won.';
+    } else if (computerMove === 'scissor') {
+      result = 'Tie.';
+    }
+  } else if (Move === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You Won.';
+    } else if (computerMove === 'paper') {
+      result = 'Tie.';
+    } else if (computerMove === 'scissor') {
+      result = 'You Lose.';
+    }
+  } else if (Move === 'rock') {
+    if (computerMove === 'rock') {
+      result = 'Tie.';
+    } else if (computerMove === 'paper') {
+      result = 'You Lose.';
+    } else if (computerMove === 'scissor') {
+      result = 'You Won.';
+    }
+  }
 
-function playerMove(Move){
-const computerMove = computerMoves();
-let result = '';
-if (Move === 'scissor'){
-if (computerMove === 'rock')
-{
-  result = 'You Lose.';
-}
-else if(computerMove === 'paper')
-{
-  result = 'You Won.'
-}
-else if(computerMove === 'scissor'){
-  result = 'Tie.';
-}
-}
-else if (Move === 'paper'){
-if (computerMove === 'rock')
-{
-  result = 'You Won.';
-}
-else if(computerMove === 'paper')
-{
-  result = 'Tie.'
-}
-else if(computerMove === 'scissor'){
-  result = 'You Lose';
-}
-}
+  if (result === 'You Won.') {
+    score.wins += 1;
+  } else if (result === 'You Lose.') {
+    score.losses += 1;
+  } else if (result === 'Tie.') {
+    score.ties += 1;
+  }
 
-else if (Move === 'rock'){
-if (computerMove === 'rock')
-{
-  result = 'Tie.';;
-}
-else if(computerMove === 'paper')
-{
-  result = 'You Lose.'
-}
-else if(computerMove === 'scissor'){
-  result = 'You Won.';
-}
+  localStorage.setItem('score', JSON.stringify(score));
+  updateScore();
+
+  document.querySelector('.result').innerHTML = `${result}`;
+
+  document.querySelector('.decision').innerHTML = 
+    `You <img class="design-icon" src="${Move}-emoji.png"> 
+     <img class="design-icon" src="${computerMove}-emoji.png"> Computer`;
 }
 
-if (result === 'You Won.')
-{
-score.wins += 1;
-}
-else if(result === 'You Lose.'){
-score.losses += 1;
-}
-else if(result === 'Tie.')
-{
-score.ties += 1;
+function updateScore() {
+  document.querySelector('.score-rsp').innerHTML = 
+    `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
 }
 
-localStorage.setItem('score',JSON.stringify(score));
-updateScore();
-
-document.querySelector('.result')
-.innerHTML =`${result}`;
-
-document.querySelector('.decision').innerHTML = 
-  `You <img class="design-icon" src="${Move}-emoji.png"> 
-   <img class="design-icon" src="${computerMove}-emoji.png"> Computer`;
-
-function updateScore(){
-
-document.querySelector('.score-rsp')
-.innerHTML =`Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}`;
-
-}
-
-function computerMoves(){
-const randomNumber = Math.random();
-let computerMove = '';
-if (randomNumber >= 0 && randomNumber < 1/3)
-{
-computerMove = 'rock';
-}
-else if(randomNumber >= 1/3 && randomNumber < 2/3) {
-computerMove = 'paper';
-}
-else if(randomNumber >= 2/3 && randomNumber <=1)
-{
-computerMove = 'scissor';
-}
-return computerMove;
+function computerMoves() {
+  const randomNumber = Math.random();
+  let computerMove = '';
+  if (randomNumber < 1 / 3) {
+    computerMove = 'rock';
+  } else if (randomNumber < 2 / 3) {
+    computerMove = 'paper';
+  } else {
+    computerMove = 'scissor';
+  }
+  return computerMove;
 }
